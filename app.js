ea2df9fc6dbb60f14a1a5fe3320c28723d88905c
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-11 02:02:29 -0300

    Add button to resize grid

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
+const root = document.querySelector(':root');
+let rootStyles = getComputedStyle(root);
+
+let size = rootStyles.getPropertyValue('--size');
+
 const grid = document.querySelector('#grid');
-grid.textContent = '';
 
-const squares = [];
+let squares = [];
 
-for (let i = 0; i < 256; i ++) {
-  squares[i] = document.createElement('div');
-  squares[i].classList.add('square');
-  squares[i].id = i;
-  grid.appendChild(squares[i]);
+function createGrid() {
+  for (let i = 0; i < size ** 2; i++) {
+    squares[i] = document.createElement('div');
+    squares[i].classList.add('square');
+    squares[i].id = i;
+    grid.appendChild(squares[i]);
+  }
+
+  for (let square of squares) {
+    square.addEventListener('mouseenter', changeColor);
+  }
 }
 
 function changeColor(e) {
-  e.target.classList.toggle('colored');
+  e.target.classList.add('colored');
 }
 
-for (let square of squares) {
-  square.addEventListener('click', changeColor);
+function setSize() {
+  do {
+    size = parseInt(prompt("Size must be between 2 and 100"));
+  }
+  while (size < 2 || size > 100 || isNaN(size));
+
+  root.style.setProperty('--size', size);
+}
+
+function createNewGrid() {
+  for (let i = 0; i < size ** 2; i++) {
+    grid.removeChild(grid.lastChild);
+  }
+
+  setSize();
+
+  createGrid();
 }
 
 function erase() {
   for (let square of squares) {
-    if (square.classList.contains('colored')) square.classList.remove('colored');
+    if (square.classList.contains('colored')) {
+      square.classList.remove('colored');
+    }
   }
 }
 
-const reset = document.getElementById('reset');
-reset.addEventListener('click', erase);
+const sizeButton = document.getElementById('size-btn');
+sizeButton.addEventListener('click', createNewGrid);
+
+const resetButton = document.getElementById('reset-btn');
+resetButton.addEventListener('click', erase);
+
+createGrid();
